1d5c3776ab14e02e7195dccb2611d340b068002a
Yori Zwols <yzwols@gmail.com>
2014-03-09 16:01:30 +0000

    Set up initial version

--- src/application.js
+++ src/application.js
@@ -2,29 +2,27 @@ var app = angular.module('lio-ng', ['ui.bootstrap', 'ui.ace', 'ui.tabs']);
 
 app.directive('resizable', function($window) {
   return function($scope) {
-    $scope.initializeWindowSize = function() {
+    var w = angular.element($window);
+
+    $scope.updateWindowSize = function() {
+      console.log("Resize");
+      console.log($window.innerWidth);
       $scope.windowHeight = $window.innerHeight;
       $scope.windowWidth  = $window.innerWidth;
     };
+
     angular.element($window).bind("resize", function() {
-      $scope.initializeWindowSize();
+      $scope.updateWindowSize();
       $scope.$apply();
     });
-    $scope.initializeWindowSize();
+
+    $scope.updateWindowSize();
   }
 });
-      
-function AppCtrl() {
-  function moveEditor() {
-    var node = document.getElementById("editor");
-    var newParent = document.getElementById("model");
-    if (newParent == null) {
-      setTimeout(moveEditor, 10);
-      return;
-    }
-    newParent.appendChild(node);
-    node.style.display = "block";
-  }
-    
-  moveEditor();
-}
+
+app.controller('AppCtrl', ['$scope', function ($scope) {
+}]);
+
+app.controller('EditorCtrl', ['$scope', function ($scope, element) {
+
+}]);
