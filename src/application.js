6aa5831cf40583227e4c3657fb0ce0d32c002d2e
Yori Zwols <yzwols@gmail.com>
2014-03-09 17:37:36 +0000

    Added glpk.js

--- src/application.js
+++ src/application.js
@@ -1,17 +1,15 @@
 var app = angular.module('lio-ng', ['ui.bootstrap', 'ui.ace', 'ui.tabs']);
 
-app.directive('resizable', function($window) {
-  return function($scope) {
-    var w = angular.element($window);
-
-    $scope.updateWindowSize = function() {
+app.directive('resizable', function ($window) {
+  return function ($scope) {
+    $scope.updateWindowSize = function () {
       console.log("Resize");
       console.log($window.innerWidth);
       $scope.windowHeight = $window.innerHeight;
-      $scope.windowWidth  = $window.innerWidth;
+      $scope.windowWidth = $window.innerWidth;
     };
 
-    angular.element($window).bind("resize", function() {
+    angular.element($window).bind("resize", function () {
       $scope.updateWindowSize();
       $scope.$apply();
     });
@@ -23,6 +21,9 @@ app.directive('resizable', function($window) {
 app.controller('AppCtrl', ['$scope', function ($scope) {
 }]);
 
-app.controller('EditorCtrl', ['$scope', function ($scope, element) {
+app.controller('EditorCtrl', ['$scope', function ($scope) {
+}]);
 
+app.controller('ButterBarCtrl', ['$scope', function ($scope) {
+  $scope.message = '';
 }]);
