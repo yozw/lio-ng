461b82427fbcda623f38ca8118fc09d5a1e03be5
Yori Zwols <yzwols@gmail.com>
2014-03-11 22:14:06 +0000

    Directives

---
+++ src/directives/resizable.js
@@ -0,0 +1,22 @@
+angular.module('ui.resizable', []);
+
+/**
+ * Directive for components that may change their size depending on the window size.
+ */
+module.directive('resizable', function ($window) {
+  "use strict"
+  return function ($scope) {
+    $scope.updateWindowSize = function () {
+      $scope.windowHeight = $window.innerHeight;
+      $scope.windowWidth = $window.innerWidth;
+    };
+
+    angular.element($window).bind("resize", function () {
+      $scope.updateWindowSize();
+      $scope.$apply();
+    });
+
+    $scope.updateWindowSize();
+  }
+});
+
---
+++ src/directives/tabs.js
@@ -0,0 +1,49 @@
+var module = angular.module('ui.tabs', []);
+
+module.directive('tabs', function () {
+  "use strict"
+  return {
+    restrict: 'E',
+    scope: {},
+    transclude: true, 
+    templateUrl: 'directives/tabs.html',
+    link: function (scope, element) {
+      scope.tabs = [];
+      scope.activeTab = 0;
+      
+      var innerDivs = element.find("div");
+      for (var i = 0; i < innerDivs.length; i++) {
+        var child = angular.element(innerDivs[i]);
+        if (child.hasClass('tab-pane')) {
+          var tab = {name: child.attr('name'), element: child};
+          scope.tabs.push(tab);
+        }
+      }
+      scope.setActiveTab(0);
+    },
+    controller: function ($scope) {
+      $scope.setActiveTab = function(index) {
+        $scope.activeTab = index;
+        for (var i = 0; i < $scope.tabs.length; i++) {
+          if (i == index) {
+            $scope.tabs[i].element.addClass('active');
+          } else {
+            $scope.tabs[i].element.removeClass('active');
+          }
+        }
+      };
+    }
+  }
+});
+
+module.directive('tabpane', function () {
+  "use strict"
+  return {
+    require: "^tabs",
+    restrict: "E",
+    transclude: true,
+    replace: true,
+    template: '<div class="tab-pane" ng-transclude></div>'
+  };
+});
+
