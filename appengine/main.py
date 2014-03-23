1b655f98062f9ea74a681ffd3d899a1411c8dc3f
Yori Zwols <yzwols@gmail.com>
2014-03-23 13:33:44 +0000

    Added appengine files

---
+++ appengine/main.py
@@ -0,0 +1,22 @@
+"""Thin lio-ng backend"""
+
+# Import the Flask Framework
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+  """Return a friendly HTTP greeting."""
+  return 'Hello World!'
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+  """Return a custom 404 error."""
+  return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+  """Return a custom 500 error."""
+  return 'Sorry, unexpected error: {}'.format(e), 500
