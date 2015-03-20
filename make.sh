34f600c26770a7753a7a3e8084cfd072fc71f320
Yori Zwols <yzwols@gmail.com>
2015-03-20 05:41:27 -0400

    Improved build scripts

--- make.sh
+++ make.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
 ROOT=`dirname $0`
 source ${ROOT}/common.sh || exit 1
-
 cd ${ROOT} || error "Could not change to directory $ROOT"
 
+
+log "Copying files ..."
 CPIO_OPT="--quiet -pdmu"
 DEST="${ROOT}/appengine/static"
-
-echo "Copying files ..."
 find src -name '*.html'   | cpio $CPIO_OPT $DEST || error "Error copying files"
 find src -name '*.css'    | cpio $CPIO_OPT $DEST || error "Error copying files"
 find lib -name '*.min.js' | cpio $CPIO_OPT $DEST || error "Error copying files"
@@ -17,5 +15,8 @@ find models -name '*.mod' | cpio $CPIO_OPT $DEST || error "Error copying files"
 find images -name '*.png' | cpio $CPIO_OPT $DEST || error "Error copying files"
 find images -name '*.gif' | cpio $CPIO_OPT $DEST || error "Error copying files"
 
+
+log "Minifying code ..."
 python minify.py || error "Could not minify code"
-echo "Done"
+
+log "Done"
