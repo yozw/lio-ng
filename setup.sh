645f62597c980bfda568900ad70dee0f22fd7411
Yori Zwols <yzwols@gmail.com>
2015-03-29 14:40:40 +0100

    Improved setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,13 @@ ROOT=`dirname $0`
 source ${ROOT}/common.sh || exit 1
 cd ${ROOT} || error "Could not change to directory $ROOT"
 
-checkDeps pip
 
-pip install -r requirements.txt -t src/python
+if [ ! -d src/python ]; then
+  checkDeps pip
+  log "Installing python requirements ..."
+  pip install -r requirements.txt -t src/python
+fi
+
 # wget -nc https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar
+
+log "Setup done"
