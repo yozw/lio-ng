db91aec74a7f909ba35884a8dff126b7c45bb8d4
Yori Zwols <yzwols@gmail.com>
2023-05-13 09:37:18 +0100

    Upgrade to Google Analytics 4

--- src/config.py
+++ src/config.py
@@ -20,12 +20,14 @@ SOURCE_DIR = getSourceDirectory()
 
 # Google Analytics code
 GA_CODE = """
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-46105838-4', 'online-optimizer.appspot.com');
-ga('send', 'pageview');
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JE7MLXZRRF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-JE7MLXZRRF');
+</script>
 """
 
 MAX_MODEL_SIZE = 128 * 1024
